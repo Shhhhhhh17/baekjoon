f44f33a9f8c7fa372b1d0c8772234ac2e91fa648
Shhhhhhh17 <psh990717@naver.com>
2025-04-04 16:06:34 +0900

    [Bronze III] Title: 최소, 최대, Time: 164 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+	int N;
+
+	scanf("%d", &N);
+
+	int num;
+	int max = -1000001;
+	int min = 1000001;
+
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d", &num);
+
+		if (num < min)
+		{
+			min = num;
+		}
+		if (num > max)
+		{
+			max = num;
+		}
+	}
+	printf("%d %d\n", min, max);
+	
+	return 0;
+}
